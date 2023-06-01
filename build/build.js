f0188f9c470af941bc2731e0f25e0c6162125ca2
petr.kalas <petr.kalas@aukro.cz>
2023-06-01 21:36:20 +0200

    third commit

--- build/build.js
+++ build/build.js
@@ -26,6 +26,7 @@ define("service/exchange.service", ["require", "exports"], function (require, ex
 define("script", ["require", "exports", "service/exchange.service"], function (require, exports, exchange_service_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    console.log('ahoj z app');
     var exchangeService = new exchange_service_1.ExchangeService();
     var moneyDto = exchangeService.exchange({ price: 20, currency: "EUR" });
     console.log(moneyDto);
